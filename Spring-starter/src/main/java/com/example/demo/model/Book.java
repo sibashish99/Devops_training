37fdc4d0a091476527c51f16196bb1ee2dc12054
sibashish99 <sibashishbiswas656@gmail.com>
2021-10-07 12:08:15 +0530

    REQ 102

--- Spring-starter/src/main/java/com/example/demo/model/Book.java
+++ Spring-starter/src/main/java/com/example/demo/model/Book.java
@@ -1,5 +1,17 @@
 package com.example.demo.model;
 
-public class Book {
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level= AccessLevel.PRIVATE)
+public class Book {
+     int bookId;
+     String bookName;
+     String author;
 }
