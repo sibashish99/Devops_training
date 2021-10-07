9576fa0278ab1e580b4a50803618b854e4713c1a
sibashish99 <sibashishbiswas656@gmail.com>
2021-10-07 12:14:33 +0530

    REQ 103 - DONE

--- Spring-starter/src/main/java/com/example/demo/service/BookService.java
+++ Spring-starter/src/main/java/com/example/demo/service/BookService.java
@@ -1,5 +1,24 @@
 package com.example.demo.service;
 
-public class BookService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.example.demo.model.Book;
+
+@Service
+public class BookService {
+	@Autowired
+   private Book book;
+	
+	public Book getBookbyId(int id) {
+		book.setBookName("java");
+		book.setBookId(101);
+		book.setAuthor("ramesh");
+		if(id==1) {
+			book.setBookName("python");
+			book.setBookId(102);
+			book.setAuthor("suresh");
+		}
+		return this.book;
+	}
 }
