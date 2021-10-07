5c83739579fd9b6479e974d05923dee2bbdf021d
sibashish99 <sibashishbiswas656@gmail.com>
2021-10-07 14:49:52 +0530

    REQ 107- Creating new project

---
+++ Spring-starter-2/src/main/java/com/example/demo/SpringStarter2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarter2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStarter2Application.class, args);
+	}
+
+}
